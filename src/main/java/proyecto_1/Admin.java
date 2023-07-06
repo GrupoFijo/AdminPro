8b270d40c6e34c595eee75e64ff94fd0d039f82f
Daniel Yali <dani852852am@gmail.com>
2023-07-05 23:29:26 -0500

    log administrador v2
    Permite ingresar contraseñas

--- src/main/java/proyecto_1/Admin.java
+++ src/main/java/proyecto_1/Admin.java
@@ -22,7 +22,7 @@ import java.sql.SQLException;
 
 /**
  *
- * @author daniel
+ * @author daniels
  */
 public class Admin {
       public List<Vendedor> vendedores= new ArrayList<>();// se necesitara una Lista dinamica para almanecar a los vendedores
